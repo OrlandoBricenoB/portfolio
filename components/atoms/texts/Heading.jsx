a47f8e950385559d938021db2340a55967fc42f4
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-24 18:16:15 -0400

    feat: Added rest parameter to Heading

--- components/atoms/texts/Heading.jsx
+++ components/atoms/texts/Heading.jsx
@@ -1,12 +1,12 @@
 import styles from '../../../styles/atoms/texts/headings.module.css'
 
-const Heading = ({ type, color, children }) => {
+const Heading = ({ type, color, children, ...rest }) => {
   const elemStyle = {
     color: color || '#ffffff'
   }
 
   return (
-    <p className={`${styles.heading} ${styles[type]}`} style={elemStyle}>
+    <p className={`${styles.heading} ${styles[type]}`} style={elemStyle} {...rest}>
       { children }
     </p>
   )
