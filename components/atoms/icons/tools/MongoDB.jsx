6280cc1fc8b334e5f8a16138ee72bbe87bf8b595
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-23 21:09:50 -0400

    feat: Added tools icons

---
+++ components/atoms/icons/tools/MongoDB.jsx
@@ -0,0 +1,34 @@
+const MongoDB = ({ size = '32' }) => {
+  return (
+    <svg width={size} height={size} viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16.62 30.5L15.869 30.251C15.869 30.251 15.969 26.451 14.594 26.184C13.694 25.136 14.727 -18.557 18.017 26.035C17.3928 26.3517 16.9151 26.8974 16.684 27.558C16.5601 28.5338 16.5387 29.5198 16.62 30.5Z" fill="url(#paint0_linear_66_158)"/>
+      <path d="M17.026 26.829C19.0256 25.3238 20.5569 23.282 21.442 20.9409C22.3271 18.5998 22.5296 16.0556 22.026 13.604C20.556 7.119 17.075 4.987 16.7 4.173C16.3765 3.66613 16.1003 3.13053 15.875 2.573L16.152 20.642C16.152 20.642 15.578 26.164 17.026 26.829Z" fill="url(#paint1_linear_66_158)"/>
+      <path d="M15.487 27.069C15.487 27.069 9.366 22.9 9.72 15.525C9.75302 13.3016 10.2628 11.1113 11.2149 9.10182C12.167 7.09237 13.5393 5.31065 15.239 3.877C15.4376 3.70825 15.5954 3.49668 15.7006 3.25818C15.8057 3.01969 15.8554 2.76047 15.846 2.5C16.227 3.32 16.165 14.743 16.205 16.079C16.36 21.276 15.916 26.088 15.487 27.069Z" fill="url(#paint2_linear_66_158)"/>
+      <defs>
+        <linearGradient id="paint0_linear_66_158" x1="11.5716" y1="16.7523" x2="20.2033" y2="19.6815" gradientUnits="userSpaceOnUse">
+          <stop offset="0.231" stop-color="#999875"/>
+          <stop offset="0.563" stop-color="#9B9977"/>
+          <stop offset="0.683" stop-color="#A09F7E"/>
+          <stop offset="0.768" stop-color="#A9A889"/>
+          <stop offset="0.837" stop-color="#B7B69A"/>
+          <stop offset="0.896" stop-color="#C9C7B0"/>
+          <stop offset="0.948" stop-color="#DEDDCB"/>
+          <stop offset="0.994" stop-color="#F8F6EB"/>
+          <stop offset="1" stop-color="#FBF9EF"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_66_158" x1="14.7369" y1="2.44388" x2="18.553" y2="26.2728" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#48A547"/>
+          <stop offset="1" stop-color="#3F9143"/>
+        </linearGradient>
+        <linearGradient id="paint2_linear_66_158" x1="9.19347" y1="16.255" x2="19.4341" y2="12.8254" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#41A247"/>
+          <stop offset="0.352" stop-color="#4BA74B"/>
+          <stop offset="0.956" stop-color="#67B554"/>
+          <stop offset="1" stop-color="#69B655"/>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default MongoDB
