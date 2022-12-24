36812bc35bc0b440238e6271e6f2f7dccb93286f
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-24 14:56:12 -0400

    feat: Added NavBar Organism

---
+++ components/organisms/Navbar.jsx
@@ -0,0 +1,33 @@
+import NavButton from '../atoms/links/NavButton'
+import styles from '../../styles/organisms/navBar.module.css'
+import Text from '../atoms/texts/Text'
+import Button from '../atoms/Button'
+
+import ContactPage from '../atoms/icons/ContactPage'
+
+const Navbar = () => {
+  return (
+    <header className={styles.header}>
+      {/* Logo */}
+      <div>
+        <Text type='big_paragraph'>Orlando Briceño</Text>
+      </div>
+
+      {/* Buttons */}
+      <div className={styles.nav_buttons}>
+        <NavButton>Inicio</NavButton>
+        <NavButton>Acerca de mí</NavButton>
+        <NavButton>Proyectos</NavButton>
+      </div>
+
+      {/* CTA */}
+      <div>
+        <Button
+          Icon={() => <ContactPage color='#070a2b' />}
+        >Contáctame</Button>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
