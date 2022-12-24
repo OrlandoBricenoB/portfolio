b8c43909cad7ccf2889f2d6614ed644182a93aef
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-24 14:55:51 -0400

    feat: Added layout in App

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import 'normalizecss/normalize.css'
 import '../styles/globals.css'
+import Layout from '../components/layouts/main'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
