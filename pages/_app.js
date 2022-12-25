598d770accf6a4e0aa9e9b109f7bfedf9396f74a
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-25 15:34:25 -0400

    feat: Added Head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import 'normalizecss/normalize.css'
 import '../styles/globals.css'
 import Layout from '../components/layouts/main'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Desarrollador Web Full Stack - Orlando Briceño</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
