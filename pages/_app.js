9ffe5846d2e38287b88d7dde557d9d5ae648e822
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-25 18:34:08 -0400

    feat: Meta viewport only for development mode.

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,14 @@ export default function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <title>Desarrollador Web Full Stack - Orlando Briceño</title>
+        {
+          process.env.NEXT_PUBLIC_APP_MODE === 'dev' && (
+            <meta
+              property="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+          )
+        }
       </Head>
       <Component {...pageProps} />
     </Layout>
