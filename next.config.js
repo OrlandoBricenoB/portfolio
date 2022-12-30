bbbbeeba37326863f5dc7e327c4f2eb1523eacb5
orlandobricenob <orlando.briceno.blanco@gmail.com>
2022-12-29 21:39:01 -0400

    feat: Includes localhost in images next

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   i18n,
   images: {
-    domains: ['orlandobricenob.dev'],
+    domains: ['localhost', 'orlandobricenob.dev'],
   },
   webpack(config) {
     config.module.rules.push({
