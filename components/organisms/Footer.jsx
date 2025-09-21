2653e008421f5df36252b4e7fed7f9a08338fc28
orlandobricenob <orlando.briceno.blanco@gmail.com>
2025-09-21 13:19:16 -0400

    feat: Make footer looks so cool

--- components/organisms/Footer.jsx
+++ components/organisms/Footer.jsx
@@ -1,6 +1,6 @@
 import useTranslate from '../../hooks/useTranslate'
 import classNames from 'classnames'
-import Container from '../atoms/Container'
+import Heading from '../atoms/texts/Heading'
 import Text from '../atoms/texts/Text'
 import Image from 'next/image'
 
@@ -14,16 +14,16 @@ const Footer = () => {
       <div className="wrapper">
         <div className={classNames('footer__content')}>
           <div className={classNames('footer__description')}>
-            <Text type='big_paragraph'>{t('footer--title')}</Text>
-            <Text type='paragraph'>
+            <Heading type='h1'>{t('footer--title')}</Heading>
+            <Text type='paragraph' style={{ fontSize: '1.1rem' }}>
               {t('footer--content-1')}
               <br /><br />
               {t('footer--content-2')}
             </Text>
           </div>
-          <div className={classNames('footer__brands')}>
-            <figure className={classNames('footer__brand')}>
-              <a href='https://gadmin.app' target='_blank' aria-label='Gadmin Website'>
+          <div className={classNames('footer__brands')} style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+            <figure className={classNames('footer__brand')} style={{ margin: 0 }}>
+              <a href='https://gadmin.app' target='_blank' aria-label='Gadmin Website' style={{ display: 'flex', alignItems: 'center', gap: '1rem', textDecoration: 'none' }}>
                 <Image
                   src={GadminLogo}
                   width={128}
@@ -32,12 +32,23 @@ const Footer = () => {
                   alt='Gadmin'
                   style={{ maxWidth: '96px', margin: '0 auto' }}
                 />
+                <span
+                  style={{
+                    fontSize: '2rem',
+                    letterSpacing: '0.1em',
+                    color: '#ffffff',
+                    userSelect: 'none',
+                    lineHeight: 1,
+                  }}
+                >
+                  Gadmin
+                </span>
               </a>
             </figure>
           </div>
         </div>
         <div className={classNames('footer__credits')}>
-          <Text type='big_paragraph' style={{ margin: 0 }}>
+        <Text type='paragraph' style={{ fontSize: '1.1rem', margin: 0 }}>
             {t('footer--credits')}
           </Text>
         </div>
